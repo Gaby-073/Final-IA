573a43efa832e58bb4397a54dab37b102baba5d3
E. Gabriela Muñoz <133884418+Gaby-073@users.noreply.github.com>
2023-08-22 14:16:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
 
+pip install vaderSentiment
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+sentiment = SentimentIntensityAnalyzer()
+
+
+text_1 = "the movie was the best movie ever"
+text_2 =  "The pizza tastes so terrible."
+text_3 = ""
+
+sent_1 = sentiment.polarity_scores(text_1)
+sent_2 = sentiment.polarity_scores(text_2)
+sent_3 = sentiment.polarity_scores(text_3)
+
+print("Sentiment of text 1:", sent_1)
+print("Sentiment of text 2:", sent_2)
+print("El sentimiento del texto 3 es:", sent_3)
